30ceb0851a5acf5cd15789a1f367f89004d71822
Kimaya <Kimaya@Kimis-MacBook-Air-2.local>
2026-03-01 12:22:14 +0530

    Add visual web interface with Express server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('\n🏊 SwiftLapLogic is running!');
+  console.log(`Open your browser: http://localhost:${PORT}\n`);
+});
